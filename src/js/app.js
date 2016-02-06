4793d0d4558494bd35a7ff000a995e3f32367f33
Yoshiki Aoki <ringohub@gmail.com>
2016-02-06 22:22:56 +0900

    Add counter component

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Counter from './counter.js';
 
 window.App = {
   render: () => {
     ReactDom.render(
-        <p>Hello world!!</p>,
-        document.getElementById('root')
+      <Counter />,
+      document.getElementById('root')
     );
   }
 };
